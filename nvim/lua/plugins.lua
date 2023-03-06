096f948e22ed952cf6d859a8ece47e4f3d84a4d7
Alexandru Ionita <alexandru.ionita@gmail.com>
2023-03-06 14:35:42 +0100

    Working Java LSP and lsp-zero
    The LSP configuration was changed to use the lsp-zero config plugin.
    This is intetnded to stream line the process of configuring the LSP
    clients. The plugin also standardizes the key bindings to be used in the
    context of LSP.
    
    The Java LSP is not managet through the lsp-zero plugin, rather with the
    mfussenegger/nvim-jdtls. The LSP server is managed using homebrew.

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,54 +1,55 @@
 local status, packer = pcall(require, "packer")
 if (not status) then
-  print("Packer is not installed")
-  return
+    print("Packer is not installed")
+    return
 end
 
 vim.cmd [[packadd packer.nvim]]
 
 packer.startup(function(use)
-  use 'wbthomason/packer.nvim'
-  use {
-    'svrana/neosolarized.nvim',
-    requires = { 'tjdevries/colorbuddy.nvim' }
-  }
+    use 'wbthomason/packer.nvim'
+    use {
+        'svrana/neosolarized.nvim',
+        requires = { 'tjdevries/colorbuddy.nvim' }
+    }
 
-  use {
-    'nvim-telescope/telescope.nvim', tag = '0.1.1',
-    -- or                            , branch = '0.1.x',
-    requires = { {'nvim-lua/plenary.nvim'} }
-  }
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.1',
+        -- or                            , branch = '0.1.x',
+        requires = { {'nvim-lua/plenary.nvim'} }
+    }
 
-  use 'norcalli/nvim-colorizer.lua'
-  use 'nvim-lualine/lualine.nvim' -- Statusline
-  use {
-      'nvim-treesitter/nvim-treesitter',  -- syntax highlighting
-      run = function()
-          local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
-          ts_update()
-      end,
-  }
+    use 'norcalli/nvim-colorizer.lua'
+    use 'nvim-lualine/lualine.nvim' -- Statusline
+    use {
+        'nvim-treesitter/nvim-treesitter',  -- syntax highlighting
+        run = function()
+            local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+            ts_update()
+        end,
+    }
+    use {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v1.x',
+        requires = {
+            -- LSP Support
+            {'neovim/nvim-lspconfig'},
+            {'williamboman/mason.nvim'},
+            {'williamboman/mason-lspconfig.nvim'},
 
-  use {
-	    'VonHeikemen/lsp-zero.nvim',
-	    branch = 'v1.x',
-	    requires = {
-    		  -- LSP Support
-    		  {'neovim/nvim-lspconfig'},
-    		  {'williamboman/mason.nvim'},
-    		  {'williamboman/mason-lspconfig.nvim'},
+            -- Autocompletion
+            {'hrsh7th/nvim-cmp'},
+            {'hrsh7th/cmp-buffer'},
+            {'hrsh7th/cmp-path'},
+            {'saadparwaiz1/cmp_luasnip'},
+            {'hrsh7th/cmp-nvim-lsp'},
+            {'hrsh7th/cmp-nvim-lua'},
 
-		      -- Autocompletion
-		      {'hrsh7th/nvim-cmp'},
-	    	  {'hrsh7th/cmp-buffer'},
-	    	  {'hrsh7th/cmp-path'},
-    		  {'saadparwaiz1/cmp_luasnip'},
-    		  {'hrsh7th/cmp-nvim-lsp'},
-    		  {'hrsh7th/cmp-nvim-lua'},
-
-		      -- Snippets
-	    	  {'L3MON4D3/LuaSnip'},
-		      {'rafamadriz/friendly-snippets'},
-	    }
-  }
+            -- Snippets
+            {'L3MON4D3/LuaSnip'},
+            {'rafamadriz/friendly-snippets'},
+        }
+    }
+    use 'mfussenegger/nvim-dap'
+    use 'mfussenegger/nvim-jdtls'
 end)

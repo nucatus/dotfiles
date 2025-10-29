c219bcb3300a65da754d6d7c6f590313b829b397
Alexandru Ionita <alexandru.ionita@gmail.com>
2025-10-29 09:31:10 +0100

    clipboard integration and line numbers

--- nvim/lua/user/options.lua
+++ nvim/lua/user/options.lua
@@ -2,5 +2,13 @@
 vim.opt.termguicolors = true
 vim.o.laststatus = 3
 
+-- System clipboard integration
+vim.opt.clipboard = "unnamedplus"
+
+-- Line numbering
+vim.opt.number = true         -- Show the absolute line number for the current line
+vim.opt.relativenumber = true -- Show relative numbers for all other lines
+
+-- set the color scheme
 vim.cmd[[colorscheme tokyonight]]
 

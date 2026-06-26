162d2954eb4b9a3e69ebb083e500fd754e185f22
Alexandru Ionita <alexandru.ionita@gmail.com>
2026-06-26 10:32:12 +0200

    Update nvim keybindings

--- nvim/lua/user/keybindings.lua
+++ nvim/lua/user/keybindings.lua
@@ -4,7 +4,23 @@ vim.g.maplocalleader = " "
 vim.keymap.set({ "n", "v" }, "<Space>", "<Nop>", { silent = true })
 
 -- FZF-Lua Keymaps
-vim.keymap.set("n", "<leader>ff", "<cmd>lua require('fzf-lua').files()<CR>", { desc = "Find Files" })
-vim.keymap.set("n", "<leader>fg", "<cmd>lua require('fzf-lua').live_grep()<CR>", { desc = "Live Grep" })
-vim.keymap.set("n", "<leader>fb", "<cmd>lua require('fzf-lua').buffers()<CR>", { desc = "Buffers" })
-vim.keymap.set("n", "<leader>fh", "<cmd>lua require('fzf-lua').help_tags()<CR>", { desc = "Help Tags" })
+vim.keymap.set("n", "<leader>f", "<cmd>lua require('fzf-lua').files()<CR>", { desc = "Find Files" })
+vim.keymap.set("n", "<leader>g", "<cmd>lua require('fzf-lua').live_grep()<CR>", { desc = "Live Grep" })
+vim.keymap.set("n", "<leader>b", "<cmd>lua require('fzf-lua').buffers()<CR>", { desc = "Buffers" })
+
+-- buffer navigation
+vim.keymap.set("n", "<Tab>", vim.cmd.bnext, { silent = true })
+vim.keymap.set("n", "<S-Tab>", vim.cmd.bprev, { silent = true })
+vim.keymap.set("n", "<leader>q", vim.cmd.bdelete, { silent = true })
+
+-- vim.rc - create a new tab, change the cwd in the context of that tab, edit key bindings
+vim.keymap.set("n", "<leader>rr", function()
+    vim.cmd("tabnew")
+    vim.cmd("tcd ~/.config/nvim")
+    vim.cmd("edit lua/user/keybindings.lua")
+end, { silent = true })
+
+vim.keymap.set("n", "<leader>rc", function()
+    vim.cmd("source $MYVIMRC")
+    print("Neovim configuration reloaded")
+end, { silent = true })
